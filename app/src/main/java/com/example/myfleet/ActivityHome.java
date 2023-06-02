ba39ddef8e71247214b31bae9b1cc5a12b590c84
LeonardoMonteiro02 <lmdsp12@gmailleonardo.pinto1@estudante.ufla.br>
2023-06-02 16:52:39 -0300

    Correção na falha do botão voltar

--- app/src/main/java/com/example/myfleet/ActivityHome.java
+++ app/src/main/java/com/example/myfleet/ActivityHome.java
@@ -1,13 +1,25 @@
 package com.example.myfleet;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myfleet.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ActivityHome extends AppCompatActivity {
-
     private BottomNavigationView bottomNavigationView;
     private BottomNavigationConfig bottomNavigationConfig;
+
+    private boolean doubleBackToExitPressedOnce = false;
+    private Handler mHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +30,34 @@ public class ActivityHome extends AppCompatActivity {
         bottomNavigationConfig = new BottomNavigationConfig(this, bottomNavigationView);
         bottomNavigationConfig.configureBottomNavigation();
 
-        // Restante do código da ActivityHome...
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            exitApp();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Pressione novamente para sair", Toast.LENGTH_SHORT).show();
+
+        mHandler = new Handler();
+        mHandler.postDelayed(mRunnable, 2000);
+    }
+
+    private Runnable mRunnable = new Runnable() {
+        @Override
+        public void run() {
+            doubleBackToExitPressedOnce = false;
+        }
+    };
+
+    private void exitApp() {
+        mHandler.removeCallbacks(mRunnable);
+        finishAffinity();
     }
 
     // Restante do código da ActivityHome...
+
 }
